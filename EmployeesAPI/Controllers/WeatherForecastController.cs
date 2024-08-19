ddcef3b56583ac80ca73ec9dfca84dd25996993f
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-08-19 13:23:28 +0200

    Add project files.

---
+++ EmployeesAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesAPI.Controllers
+{
+    [ApiController]
+    [Route("Weather")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
